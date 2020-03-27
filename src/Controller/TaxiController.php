8724356242467a318bde0e099643bd23e66a0ad4
Artem Harlamenko <4.harlamenko@gmail.com>
2020-03-27 21:56:19 +0300

    feature: update-taxi api

--- src/Controller/TaxiController.php
+++ src/Controller/TaxiController.php
@@ -55,4 +55,36 @@ class TaxiController
 
         return $response;
     }
+    
+    public function update(Request $request, ValidatorInterface $validator) {
+        $aid = $request->query->get('aid');
+        $content = $request->getContent();
+        $result;
+        $code;
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+
+        if (array_key_exists($aid, Taxi::$stubObjects)) {
+            $json_decodedContent = json_decode($content);
+            $object = new MTaxi($json_decodedContent);
+            $errors = $validator->validate($object);
+        
+            if (count($errors)) {
+                $humanized_errors = Validator::humanize($errors);
+                $code = Response::HTTP_BAD_REQUEST;
+                $result = json_encode($humanized_errors);
+            } else {
+                $code = Response::HTTP_OK;
+                $result = '"Ok"';
+            }
+        } else {
+            $code = Response::HTTP_NOT_FOUND;
+            $result = '"Not Found"';
+        }
+
+        $response->setContent($result);
+        $response->setStatusCode($code);
+
+        return $response;
+    }
 }
